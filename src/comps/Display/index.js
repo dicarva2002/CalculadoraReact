9364290b8f7a59a963b2003b3097de24e891fb72
dicarva2002 <diogocarvalho117@gmail.com>
2021-12-03 10:16:49 +0000

    primeira atualizacao

---
+++ src/comps/Display/index.js
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import PropTypes from "prop-types";
+import "./index.css"
+
+export default class Display extends React.Component {
+    static propTypes = {
+        value: PropTypes.string,
+    }
+    render() {
+        return (
+            <div className="component-display">
+                <div>{this.props.value}</div>
+            </div>
+        )
+    }
+}
---
+++ src/logic/calculate.js
@@ -0,0 +1,112 @@
+import IsNumber from "./isNumber"
+import operate from "./operate";
+import Big from "big.js"
+
+export default function Calculate(obj, buttonName) {
+    if (buttonName === "AC") {
+        return {
+            total: null,
+            next: null,
+            operation: null,
+        }
+    }
+    if (IsNumber(buttonName)) {
+        if (buttonName === "0" && obj.next === "0") {
+            return {}
+        }
+
+        if (obj.operation) {
+            if (obj.next) {
+                return { next: obj.next + buttonName };
+            }
+            return { next: buttonName };
+        }
+        //sem operação associada
+        if (obj.next) {
+            const next = obj.next === "0" ? buttonName : obj.next + buttonName;
+            return {
+                next,
+                total: null,
+            };
+        }
+        return {
+            next: buttonName,
+            total: null,
+        };
+    }
+    //percentagem
+    if (buttonName === "%") {
+        if (obj.operation && obj.next) {
+            const result = operate(obj.total, obj.next, obj.operation);
+            return {
+                total: Big(result)
+                    .div(Big(100)).toString,
+                next: null,
+                operation: null,
+            };
+        }
+        if (obj.next) {
+            return {
+                next: Big(obj.next).div(Big("100")).toString(),
+            };
+        }
+        return {};
+    }
+    //tratar botao .
+    if (buttonName === ".") {
+        if (obj.next) {
+            if (obj.next.includes(".")) {
+                return {}
+            }
+            return { next: obj.next + "." };
+        }
+        return { next: "0." }
+    }
+
+    //botao igual
+    if (buttonName === "=") {
+        if (obj.next && obj.operation) {
+            return {
+                total: operate(obj.total, obj.next, obj.operation),
+                next: null,
+                operation: null,
+            }
+        }
+        else {
+            return {}
+        }
+    }
+
+    // botao "+/-"
+    if (buttonName === "+/-") {
+        if (obj.next) {
+            return {
+                next: (-1 * parseFloat(obj.next).toString())
+            }
+        }
+        if (obj.total) {
+            return {
+                total: (-1 * parseFloat(obj.total).toString())
+            }
+
+        }
+        return {};
+    }
+
+    //operacao
+    if (obj.operation) {
+        return {
+            total: operate(obj.total, obj.next, obj.operation),
+            next: null,
+            operation: buttonName,
+        };
+    }
+    if (!obj.next) {
+        return { operation: buttonName, };
+    }
+    return {
+        total: obj.next,
+        next: null,
+        operation: buttonName,
+    };
+}
